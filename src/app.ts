a8bc888b534c65410b4a41abc19800d29cd108ed
Uziel Tzab <uzieltzab8@gmail.com>
2025-04-11 21:49:36 -0600

    Se añadió los middlewares para el parseo de json

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 import cors from 'cors';
 import userRouter from './routes/user.routes';
 import { connectToDatabase } from './db/db.config';
@@ -10,6 +11,8 @@ const PORT = process.env.PORT || 3004;
 connectToDatabase();
 
 app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
 app.use("/dekut_games", userRouter)
