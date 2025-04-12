5b5c3c1468e8f21ecb93c47f1c9af2f69228c9bb
Uziel Tzab <uzieltzab8@gmail.com>
2025-04-12 17:05:32 -0600

    Cambio en .gitignore

--- dist/routes/user.routes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const user_controller_1 = require("../controllers/user.controller");
-const userRouter = (0, express_1.Router)();
-userRouter.get("/users", user_controller_1.getAllUsers);
-userRouter.post("/users", user_controller_1.createUser);
-userRouter.post("/users/login", user_controller_1.authenticateUser);
-exports.default = userRouter;
